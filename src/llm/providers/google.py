704436dc03527070f478b78ea9953af9e0b3b24c
Kamil Rybacki <kamilandrzejrybacki@gmail.com>
2024-06-17 14:54:42 +0200

    add google ai lib integration tests

---
+++ src/llm/providers/google.py
@@ -0,0 +1,101 @@
+# pylint: disable=no-member
+import abc
+import dataclasses
+import logging
+import time
+import typing
+
+import google.generativeai.client
+
+from llm.base import LLMProvider, LLMQueryResult, TokenCounters
+
+
+# Limits below are taken from: https://aistudio.google.com/app/plan_information
+
+MAX_TPM_PER_MODEL: dict[str, int] = {
+    "gemini-1.0-pro": 32_000,
+    "gemini-1.5-flash": 1_000_000,
+}
+
+MAX_RPM_PER_MODEL: dict[str, int] = {
+    "gemini-1.0-pro": 2,
+    "gemini-1.5-flash": 15,
+}
+
+
+@dataclasses.dataclass
+class GoogleAI(LLMProvider, abc.ABC):
+    _model: google.generativeai.GenerativeModel
+    _limits: dict[str, int] = dataclasses.field(default_factory=dict)
+    _usage_counters: dict[str, int] = dataclasses.field(default_factory=dict)
+    _last_check_time: float = dataclasses.field(init=False, default=0)
+
+    @property
+    @abc.abstractmethod
+    def model(self) -> str:
+        pass
+
+    @classmethod
+    def connect(cls, config: dict[str, str]) -> LLMProvider:
+        google.generativeai.configure(
+            api_key=config.get("api_key"),
+        )
+        return cls(
+            _model=google.generativeai.GenerativeModel(
+                model_name=cls.model,
+            ),
+            _limits={
+                'TPM': MAX_TPM_PER_MODEL[cls.model],  # type: ignore
+                'RPM': MAX_RPM_PER_MODEL[cls.model],  # type: ignore
+            },
+            _usage_counters={
+                'TPM': 0,
+                'RPM': 0,
+            }
+        )
+
+    def query(self, query: str) -> LLMQueryResult:
+        tokens_to_send = self.count_tokens(query)
+        if self._are_limits_exceeded(tokens_to_send):
+            self.log("Usage limits exceeded. Waiting for the next minute to continue.", level=logging.WARNING)
+            time.sleep(60)
+        query_response = self._model.generate_content(query)
+        result = LLMQueryResult(
+            query=query,
+            result=query_response.text,
+            tokens=TokenCounters(
+                prompt=tokens_to_send,
+                total=query_response.usage_metadata.total_token_count,
+            )
+        )
+        self._update_usage_counters(result.tokens)
+        return result
+
+    def _are_limits_exceeded(self, requested_tokens: int) -> bool:
+        current_time = time.time()
+        if current_time - self._last_check_time > 60:
+            self._usage_counters['TPM'] = 0
+            self._usage_counters['RPM'] = 0
+            self._last_check_time = current_time
+        if self._usage_counters['TPM'] >= self._limits['TPM']:
+            return True
+        if self._usage_counters['TPM'] + requested_tokens > self._limits['TPM']:
+            return True
+        return not self._usage_counters['RPM'] < self._limits['RPM']
+
+    def _update_usage_counters(self, tokens: TokenCounters) -> None:
+        self._usage_counters['TPM'] += tokens['total']
+        self._usage_counters['RPM'] += 2
+
+    def count_tokens(self, query: str) -> int:
+        return self._model.count_tokens(query).total_tokens
+
+
+@dataclasses.dataclass
+class GeminiOnePro(GoogleAI):
+    model: str = "gemini-1.0-pro"
+
+
+@dataclasses.dataclass
+class GeminiOneHalfFlash(GoogleAI):
+    model: str = "gemini-1.5-flash"
---
+++ tests/providers/test_google_provider.py
@@ -0,0 +1,126 @@
+import math
+import logging
+import random
+import string
+
+import google.generativeai
+from google.generativeai.types.generation_types import BaseGenerateContentResponse
+from google.generativeai.protos import GenerateContentResponse, CountTokensResponse
+import pytest
+
+import llm.base
+import llm.providers.google
+import llm.setup
+
+
+class MockGenerativeModel(google.generativeai.GenerativeModel):
+    def generate_content(self, prompt: str) -> BaseGenerateContentResponse:
+        return BaseGenerateContentResponse(
+            done=True,
+            result=GenerateContentResponse(
+                usage_metadata=GenerateContentResponse.UsageMetadata(
+                    total_token_count=len(prompt.split())
+                ),
+            ),
+            iterator=None
+        )
+
+    def count_tokens(self, prompt: str) -> CountTokensResponse:
+        return CountTokensResponse(
+            total_tokens=len(prompt.split())
+        )
+
+
+# pylint: disable=all
+class MockGoogleAI(llm.providers.google.GoogleAI):
+    model: str = "mockini-1.0-pro"
+    _tokens_to_send: int = 0
+
+    @classmethod
+    def connect(cls, config: dict[str, str]) -> llm.base.LLMProvider:
+        return cls(
+            _model=MockGenerativeModel(model_name=cls.model),
+            _limits={
+                'TPM': 0,
+                'RPM': 0,
+            },
+            _usage_counters={
+                'TPM': 0,
+                'RPM': 0,
+            }
+        )
+
+    def query(self, query: str) -> llm.base.LLMQueryResult:
+        if self._are_limits_exceeded(
+            self._tokens_to_send
+        ):
+            raise ValueError("Usage limits exceeded")
+        usage_data = llm.base.TokenCounters(
+            prompt=self._tokens_to_send,
+            total=self._tokens_to_send
+        )
+        self._update_usage_counters(usage_data)
+        return llm.base.LLMQueryResult(
+            query=query,
+            result="Mocked result",
+            tokens=usage_data
+        )
+
+
+@pytest.mark.parametrize(
+    "limit, which_limit",
+    [
+        (
+            random.randint(1, 100),
+            random.choice(['TPM', 'RPM'])
+        )
+        for _ in range(100)
+    ]
+)
+def test_token_limiter(
+    limit: int,
+    which_limit: str,
+) -> None:
+    provider = MockGoogleAI.connect({})
+    provider._limits = {
+        'TPM': limit if which_limit == 'TPM' else math.inf,
+        'RPM': limit if which_limit == 'RPM' else math.inf
+    }
+    provider._tokens_to_send = limit + 1 if which_limit == 'TPM' else 1
+    iterations = limit + 1 if which_limit == 'RPM' else 1
+    with pytest.raises(ValueError):
+        for _ in range(iterations):
+            provider.query("Some query")
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        model
+        for model in llm.setup.AVAILABLE_PROVIDERS.keys()
+        if model.startswith("G")
+    ]
+)
+def test_google_model(name: str, monkeypatch) -> None:
+    fake_api_key = "AI" + ''.join(
+        random.choice(string.ascii_letters + string.digits)
+        for _ in range(37)
+    )
+    provider_config = {
+        "name": name,
+        "connection": {
+            "api_key": fake_api_key
+        },
+        "retry": {
+            "delay": 1,
+            "retries": 3
+        }
+    }
+    monkeypatch.setattr(
+        "google.generativeai.configure",
+        lambda api_key: logging.info(f"Configured with mock api key: {api_key}")
+    )
+    provider: llm.providers.google.GoogleAI = llm.setup.get_provider(provider_config)
+    provider._model = MockGenerativeModel(model_name=provider.model)
+    with pytest.raises(ValueError):
+        provider.query("Some query")
