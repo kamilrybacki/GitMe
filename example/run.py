165ef8d542abf514644b1c064035ed1c42297763
Kamil Rybacki <krybacki@task.gda.pl>
2024-06-18 14:05:56 +0200

    simpler runner

--- example/run.py
+++ example/run.py
@@ -1,61 +1,34 @@
 import os
 
-import gitme.gh
-import gitme.llm.setup
-import gitme.llm.prompts
-import gitme.metadata
+from gitme.runner import GitMeRunner
 
 
-def start() -> None:
-    username = os.getenv("GITME__GITHUB_USERNAME")
-    token = os.getenv("GITME__GITHUB_TOKEN")
-    if not username or not token:
-        raise ValueError("Please provide GITHUB_USERNAME and GITHUB_TOKEN environment variables")
-    profile = gitme.gh.GithubProfile.connect(username, token)
-    profile.log("Successfully connected to Github!")
+if __name__ == '__main__':
+    gh_username = os.getenv("GITME__GITHUB_USERNAME")
+    gh_token = os.getenv("GITME__GITHUB_TOKEN")
+    if not gh_username or not gh_token:
+        raise ValueError("Please provide GITME__GITHUB_USERNAME and GITME__GITHUB_TOKEN environment variables")
+    if not (llm_api_key := os.getenv("GITME__LLM_PROVIDER_API_KEY")):
+        raise ValueError("Please provide GITME__LLM_PROVIDER_API_KEY environment variable")
 
-    llm_client = gitme.llm.setup.get_provider({
-        "name": "G1HF",
-        "connection": {
-            "api_key": os.getenv("GITME__LLM_PROVIDER_API_KEY", "")
+    runner = GitMeRunner({
+        "llm": {
+            "name": "G1HF",
+            "connection": {
+                "api_key": llm_api_key
+            },
+            "retry": {
+                "delay": 5,
+                "attempts": 3
+            }
         },
-        "retry": {
-            "delay": 1,
-            "retries": 3
-        }
+        "github": {
+            "username": gh_username,
+            "token": gh_token,
+            "only": os.getenv("GITME__ONLY_THOSE_REPOS"),
+            "add": os.getenv("GITME__EXTRA_REPOS")
+        },
+        "output": "/tmp/output.csv"
     })
-    llm_client.set_logger(profile.logger)
-
-    additional_repos = []
-    if requested_repos := os.getenv("GITME__EXTRA_REPOS"):
-        additional_repos = requested_repos.split(",") if "," in requested_repos else [requested_repos]
-        profile.log(f"Additional repos to be analyzed: {requested_repos}")
-
-    all_repositories = [
-        *profile.pinned_repositories, 
-    ] + [
-            gitme.metadata.RepositoryMetadata.from_repo(
-                profile.get_repo(repo_name)
-            )
-            for repo_name in additional_repos
-            if repo_name
-    ] if additional_repos else [*profile.pinned_repositories]
-    profile.log(f"Repositories: {', '.join([
-        repo.name
-        for repo in all_repositories
-    ])}")
-
-    for repo in all_repositories[:1]:
-        profile.log(f"Processing {repo.name}")
-        prompt = gitme.llm.prompts.generate_prompt(
-            description=repo.description,
-            technologies=repo.technologies,
-            readme=repo.readme or "No README available. Use the repository description."
-        )
-        summary = llm_client.query(prompt)
-        llm_client.log(f"Query: {summary.query}")
-        llm_client.log(f"Result: {summary.result}")
-
-
-if __name__ == "__main__":
-    start()
+    analyzed_data = runner.run()
+    runner.dump(analyzed_data)
--- example/run.sh
+++ example/run.sh
@@ -3,7 +3,11 @@
 export GITME__GITHUB_USERNAME="your_username"
 export GITME__GITHUB_TOKEN="your_token"
 export GITME__LLM_PROVIDER_API_KEY="your_api_key"
-export GITME__EXTRA_REPOS="repo1,repo2,repo3"
+
+# These are optional and either limit the analysis to a subset of repos or add extra repos to the pinned ones. ONLY_THOSE_REPOS takes precedence.
+
+# export GITME__EXTRA_REPOS="repo1,repo2,repo3"
+# export GITME__ONLY_THOSE_REPOS="repo1,repo2,repo3"
 
 python \
     run.py
